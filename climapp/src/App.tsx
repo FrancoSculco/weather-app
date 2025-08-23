49522869f5c8fb819bb240cc7c2b3574c34fb5fb
FrancoSculco <franco.sculco14@hotmail.com>
2025-08-22 21:00:29 -0300

    first push

---
+++ climapp/src/App.tsx
@@ -0,0 +1,165 @@
+import { LoadingButton } from "@mui/lab";
+import { Box, Container, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=`;
+
+interface Weather {
+  city: string;
+  country: string;
+  temp: number | '';
+  condition: number | '';
+  icon: string;
+  conditionText: string;
+}
+
+interface ErrorState {
+  error: boolean;
+  message: string;
+}
+
+export default function App() {
+  const [city, setCity] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<ErrorState>({
+    error: false,
+    message: '',
+  });
+
+  const [weather, setWeather] = useState<Weather>({
+    city: '',
+    country: '',
+    temp: '',
+    condition: '',
+    icon: '',
+    conditionText: '',
+  });
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    setError({
+      error: false,
+      message: "",
+    });
+
+    try {
+      if (!city.trim()) throw { message: "El campo ciudad es obligatorio" };
+
+      const response = await fetch(`${API_WEATHER}${city}`);
+
+      const data = await response.json();
+
+      if (data.error) throw { message: data.error.message };
+
+      setWeather({
+        city: data.location.name,
+        country: data.location.country,
+        temp: data.current.temp_c,
+        condition: data.current.condition.code,
+        icon: data.current.condition.icon,
+        conditionText: data.current.condition.text,
+      });
+
+    } catch (error: unknown) {
+      if (typeof error === "object" && error !== null && "message" in error) {
+        setError({
+          error: true,
+          
+          message: (error as { message: string }).message,
+        });
+      } else {
+        setError({
+          error: true,
+          message: "Error desconocido",
+        });
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Container
+      maxWidth='xs'
+      sx={{ mt: 2 }}
+    >
+      <Typography
+        variant="h3"
+        component='h1'
+        align="center"
+        gutterBottom
+      >
+        Weather App
+      </Typography>
+
+      <Box
+        sx={{ display: 'grid', gap: 2 }}
+        component='form'
+        autoComplete="off"
+        onSubmit={onSubmit}
+      >
+        <TextField
+          id="city"
+          label='Ciudad'
+          variant="outlined"
+          size="small"
+          fullWidth
+          value={city}
+          onChange={e => setCity(e.target.value)}
+          error={error.error}
+          helperText={error.message}
+        />
+
+        <LoadingButton
+          type="submit"
+          variant="contained"
+          loading={loading}
+          loadingIndicator='Cargando...'
+        >
+          Buscar
+        </LoadingButton>
+      </Box>
+
+      {weather.city && (
+        <Box
+          sx={{ mt: 2, display: 'grid', gap: 2, textAlign: 'center' }}
+        >
+          <Typography variant="h4" component='h2'>
+            {weather.city}, {weather.country}
+          </Typography>
+
+          <Box
+            component='img'
+            alt={weather.conditionText}
+            src={weather.icon}
+            sx={{ margin: '0 auto' }}
+          />
+
+          <Typography variant="h5" component='h3'>
+            {weather.temp} °C
+          </Typography>
+
+          <Typography variant="h5" component='h4'>
+            {weather.conditionText}
+          </Typography>
+        </Box>
+      )}
+
+      <Typography
+        textAlign='center'
+        sx={{ mt: 2, fontSize: '10px' }}
+      >
+        Powered by: {" "}
+
+        <a
+          href="https://www.weatherapi.com/"
+          title="Weather API"
+        >
+          WeatherAPI.com
+        </a>
+      </Typography>
+
+    </Container>
+  )
+}
